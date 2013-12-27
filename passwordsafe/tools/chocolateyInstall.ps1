236b72405bcb88a955c8fe08a5c8f81586b596db
Jim Schell <jim@jschell.com>
2013-12-26 20:36:35 -0800

    first two packages created
    passwordSafe and smplayer packages created - validating against azure
    instance....

---
+++ passwordsafe/tools/chocolateyInstall.ps1
@@ -0,0 +1,1 @@
+install-ChocolateyPackage -packageName 'passwordsafe' -fileType 'exe' -silentArgs '/s' -url  'http://sourceforge.net/projects/passwordsafe/files/passwordsafe/3.32/pwsafe-3.32.exe/download'
---
+++ smplayer/tools/chocolateyInstall.ps1
@@ -0,0 +1,1 @@
+install-chocolateyPackage -packageName 'smplayer' -fileType 'exe' -silentArgs '/s' -url 'http://www.fosshub.com/SMPlayer.html/smplayer-0.8.6-ps-win32.exe'
