cfb3c71a67ff4574400451f58a34703034bf251b
Jim Schell <jim@jschell.com>
2013-12-26 21:23:47 -0800

    changed to using the msi installer for password safe
    attempting to get silent install to function as expected

--- passwordsafe/tools/chocolateyInstall.ps1
+++ passwordsafe/tools/chocolateyInstall.ps1
@@ -1,1 +1,1 @@
-install-ChocolateyPackage -packageName 'passwordsafe' -fileType 'exe' -silentArgs '/s' -url  'http://sourceforge.net/projects/passwordsafe/files/passwordsafe/3.32/pwsafe-3.32.exe/download'
+install-ChocolateyPackage -packageName 'passwordsafe' -fileType 'msi' -silentArgs '/quiet' -url  'http://sourceforge.net/projects/passwordsafe/files/passwordsafe/3.32/pwsafe-3.32.msi/download'
