f240b433b8e2760687830a05f164876812af5aee
Jim Schell <jim@jschell.com>
2013-12-31 12:26:19 -0800

    smplayer package confirmed working x64
    appears there was an issue with pulling the x64 pkg from the SF mirror.
    adjusted to use the basic SF link style, as recommended in the
    autoUpdate wiki
    https://github.com/chocolatey/chocolatey/wiki/AutomaticPackages

--- smplayer/tools/chocolateyInstall.ps1
+++ smplayer/tools/chocolateyInstall.ps1
@@ -1,1 +1,1 @@
-install-chocolateyPackage -packageName 'smplayer' -fileType 'exe' -silentArgs '/S' -url 'http://www.fosshub.com/SMPlayer.html/smplayer-0.8.6-ps-win32.exe' -url64bit 'http://www.fosshub.com/SMPlayer.html/smplayer-0.8.6-x64.exe'
+install-chocolateyPackage -packageName 'smplayer' -fileType 'EXE' -silentArgs '/S' -url 'http://sourceforge.net/projects/smplayer/files/SMPlayer/0.8.6/smplayer-0.8.6-win32.exe/download' -url64bit 'http://sourceforge.net/projects/smplayer/files/SMPlayer/0.8.6/smplayer-0.8.6-x64.exe/download'
