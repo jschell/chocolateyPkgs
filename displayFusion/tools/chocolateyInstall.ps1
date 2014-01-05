9a5015b313d6c25fdbab98b08a1fd1c9c12ab00e
Jim Schell <jim@jschell.com>
2014-01-04 20:18:28 -0800

    added displayFusion package

---
+++ displayFusion/tools/chocolateyInstall.ps1
@@ -0,0 +1,32 @@
+function get-osArch
+{
+    $ptr = [intptr]::size
+    if($ptr -eq 8)
+    {
+        $os = 64
+    }
+    else
+    {
+        $os = 32
+    }
+
+    return $os
+}
+
+$arch = get-osArch
+if($arch -eq 64 )
+{
+    $progFile = "$env:programFiles (x86)"
+}
+else
+{
+    $progFile = "$env:programFiles"
+}
+
+$dirPath = "\DisplayFusion"
+$installPath = $progFile + $dirPath
+
+$localeTwoLetter = (Get-Culture).TwoLetterISOLanguageName
+$silentOptions = "/VERYSILENT /LANG=$localeTwoLetter /DIR=$installPath /STARTUPALL=1 /MERGETASKS=`"!desktopicon,!desktopicon\common,!desktopicon\user`" "
+
+install-ChocolateyPackage -packageName 'displayFusion' -fileType 'exe' -silentArgs $silentOptions -url  'www.binaryfortress.com/Data/Download/?package=displayfusion'
