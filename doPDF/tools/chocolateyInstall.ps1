b045c495a21ae305186ee91ec329da0ee292201f
Jim Schell <jim@jschell.com>
2015-01-01 21:55:19 -0800

    fixed the terrible silent install garbage for 7.3
    no idea when 8+ silent will get fixed

--- doPDF/tools/chocolateyInstall.ps1
+++ doPDF/tools/chocolateyInstall.ps1
@@ -1,3 +1,6 @@
-$silentOptions = "/VERYSILENT /NORESTART"
+﻿$packageName = 'doPDF'
+$fileType = 'EXE'
+$silentArgs = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
+$url = 'http://www.dopdf.com/download/setup/dopdf_7_3_400.exe'
 
-install-ChocolateyPackage -packageName 'doPDF' -fileType 'exe' -silentArgs $silentOptions -url  'http://www.dopdf.com/download/setup/dopdf-7.exe'
+install-ChocolateyPackage -packageName $packageName -fileType $fileType -silentArgs $silentArgs -url $url
