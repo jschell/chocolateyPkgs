02a1004af338751b0e13076652af2852533ceb7c
Jim Schell <jim@jschell.com>
2015-01-01 17:00:34 -0800

    updated to preferred install script pattern
    sample from -
    https://github.com/chocolatey/chocolatey-coreteampackages/blob/master/automatic/fiddler4/tools/chocolateyInstall.ps1

--- smplayer/tools/chocolateyInstall.ps1
+++ smplayer/tools/chocolateyInstall.ps1
@@ -1,1 +1,7 @@
-install-chocolateyPackage -packageName 'smplayer' -fileType 'EXE' -silentArgs '/S' -url 'http://downloads.sourceforge.net/project/smplayer/SMPlayer/14.9.0/smplayer-14.9.0-win32.exe' -url64bit 'http://downloads.sourceforge.net/project/smplayer/SMPlayer/14.9.0/smplayer-14.9.0-x64.exe'
+$packageName = 'smplayer'
+$fileType = 'EXE'
+$silentArgs = '/S'
+$url32bit = 'http://downloads.sourceforge.net/project/smplayer/SMPlayer/14.9.0/smplayer-14.9.0-win32.exe'
+$url64bit = 'http://downloads.sourceforge.net/project/smplayer/SMPlayer/14.9.0/smplayer-14.9.0-x64.exe'
+
+install-chocolateyPackage -packageName $packageName -fileType $fileType -silentArgs $silentArgs -url $url32bit -url64bit $url64bit
